1111eed47b9da306aedf00e7bfba24ebb4982b1f
kermanshahianAli <kermanshahianali@gmail.com>
2022-05-06 18:28:12 +0430

    a program to practice Conditions

---
+++ grades.py
@@ -0,0 +1,43 @@
+grade = float(input("enter the grade : "))
+
+if grade<0 or 20<grade :
+    print("invalid grade . . .")
+elif 19<=grade :
+    print("A grade")
+elif 17<=grade :
+    print("B grade")
+elif 15<=grade :
+    print("good")
+elif 0<=grade :
+    print("Fail")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
