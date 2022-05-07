60988f7db747fea31aa1e3a414b4e23f1b5aa7f1
kermanshahianAli <kermanshahianali@gmail.com>
2022-05-07 13:30:34 +0430

    a program that calculate distance between two points

---
+++ distance.py
@@ -0,0 +1,8 @@
+from math import sqrt
+def distance(x1,y1,x2,y2):
+    d = sqrt( (x2-x1)**2 + (y2-y1)**2 )       
+    print("The Distance Between (x1,y1) and (x2,y2) :" , d)
+
+distance(float(input("x1= ")),float(input("y1= ")),float(input("x2= ")),float(input("y2= ")))
+#thanks for your time ;)
+
