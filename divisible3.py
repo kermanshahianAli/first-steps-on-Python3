ee0d3eb702339b600412ed8ec5ef3e7aa84acfe1
kermanshahianAli <kermanshahianali@gmail.com>
2022-05-07 13:31:55 +0430

    calculate the n number divisible by 3

---
+++ divisible3.py
@@ -0,0 +1,9 @@
+def divisible3(num):
+    if num%3==0:
+        print(num , "is divisible by 3",'\n')
+    else:
+        print(num , "is not divisible by 3",'\n')
+
+num = int(input("number: "))
+divisible3(num)
+#thanks for your time ;)
